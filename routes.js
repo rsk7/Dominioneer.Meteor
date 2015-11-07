d87bf1f10a31dd62f66b6a0575d10d04fade7041
rsk7 <ravikrishnasingh@gmail.com>
2015-11-07 11:57:17 +0545

    updating route syntax

--- routes.js
+++ routes.js
@@ -2,24 +2,8 @@ Router.configure({
   layoutTemplate: "appLayout"
 });
 
-Router.route("/games", function() {
-  this.render("games");
-});
-
-Router.route("/history", function() {
-	this.render("history");
-});
-
-Router.route("/unrated", function() {
-	this.render("unrated");
-});
-
-Router.route("/settings", function() {
-	this.render("settings");
-});
-
-Router.route("/", function() {
-	this.render("games");
-});
-
-
+Router.route("/", { template: "games" });
+Router.route("/games");
+Router.route("/history");
+Router.route("/unrated");
+Router.route("/settings");
