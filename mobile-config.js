4869bec65dc6d00d3374ac6290c3775871b0d441
rsk7 <ravikrishnasingh@gmail.com>
2015-11-08 12:20:02 +0545

    setting up facebookconnect

--- mobile-config.js
+++ mobile-config.js
@@ -1,8 +1,13 @@
 App.setPreference('android-targetSdkVersion', '22');
+App.accessRule("*");
 
 App.configurePlugin('com.phonegap.plugins.facebookconnect', {
-  APP_ID: Meteor.settings.facebook.AppId,
-  APP_NAME: Meteor.settings.facebook.AppName
+  APP_ID: 973648849342797,
+  APP_NAME: "Dominioneer"
 });
 
-
+App.info({
+  id: "com.meteor.dominioneer"
+  name: "Dominioneer",
+  description: "Dominioneer"
+});
